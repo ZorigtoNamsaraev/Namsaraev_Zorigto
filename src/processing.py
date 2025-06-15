535ec41c95ec629a1edc545fdcb28a7fa7fe0920
Nzori <nzorigto@bk.ru>
2025-06-15 17:57:58 +0300

    new def

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,10 @@
 def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """ Фильтрует список словарей по значению ключа 'state' """
     return [item for item in data if item.get('state') == state]
+
+
+from datetime import datetime
+
+def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
+    """ Сортирует список словарей по полю 'date' """
+    return sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
